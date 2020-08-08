761829337c98874f2d191ab7949f327115f25479
Loïc <lolo.mess.17@gmail.com>
2020-08-08 19:19:38 +0200

    Multi files + css gallery & header

--- public/resources/index.js
+++ public/resources/index.js
@@ -5,17 +5,19 @@ socket.on('new picture', pic)
 
 //Append picture function
 function pic(base64Image) {
-    $('#gallery').append($('<div>'), '<img src="' + base64Image + '"/>')
+    $('#gallery').append($('<img src="' + base64Image + '"/>'))
 }
 
 $(function () {
     //Input event
     $('#picfile').bind('change', function (e) {
-        var data = e.originalEvent.target.files[0]
-        var reader = new FileReader()
-        reader.onload = function (evt) {
-            socket.emit('new picture', evt.target.result)
+        for (let i = 0;i<e.originalEvent.target.files.length;i++) {
+            var data = e.originalEvent.target.files[i]
+            var reader = new FileReader()
+            reader.onload = function (evt) {
+                socket.emit('new picture', evt.target.result)
+            }
+            reader.readAsDataURL(data)
         }
-        reader.readAsDataURL(data)
     })
 })
